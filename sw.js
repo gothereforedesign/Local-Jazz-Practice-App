412a33bad2c1cbeeafd0bf13354e8bfe2503adf8
gothereforedesign <gothereforedesign@gmail.com>
2025-12-22 22:22:06 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = 'jazz-cache-v1';
+const CACHE_NAME = 'jazz-v2';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
