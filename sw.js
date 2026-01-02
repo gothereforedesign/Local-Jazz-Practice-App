2396fe51fe5ebbf37745252302f99344bc3951d0
gothereforedesign <gothereforedesign@gmail.com>
2026-01-02 05:02:22 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
 const CACHE_NAME = 'jazz-practice-v1';
-const ASSETS = [
+const urlsToCache = [
   './',
-  'index.html',
-  'manifest.json',
-  'https://cdn.tailwindcss.com',
+  './index.html',
+  './manifest.json',
+  // Cache External CDNs
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://unpkg.com/lucide@latest'
+  'https://cdn.tailwindcss.com'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
